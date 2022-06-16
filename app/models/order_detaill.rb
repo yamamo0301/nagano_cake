213d302547d54573d2b49be4560b0399fc0f85f0
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-16 14:10:33 +0000

    [create]tables

---
+++ app/models/order_detaill.rb
@@ -0,0 +1,2 @@
+class OrderDetaill < ApplicationRecord
+end
---
+++ db/migrate/20220616140406_create_order_detaills.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetaills < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_detaills do |t|
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
