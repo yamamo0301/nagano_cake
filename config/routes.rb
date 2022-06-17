52731e30910fb033b4533cffa9bc258e7be43b84
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-17 21:40:57 +0000

    [gem]refile install

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
 end
