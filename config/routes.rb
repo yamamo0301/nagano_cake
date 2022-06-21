b5b1cead1cf2b7cfef92887ce96921877e334d4e
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-21 22:22:19 +0000

    [create]top

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
 end
