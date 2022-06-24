b780fcacc21999485fbbbf1f239e88b835e8af69
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-24 13:04:27 +0000

    [create]customer/sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_215819) do
+ActiveRecord::Schema.define(version: 2022_06_22_113014) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -63,6 +63,11 @@ ActiveRecord::Schema.define(version: 2022_06_17_215819) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
