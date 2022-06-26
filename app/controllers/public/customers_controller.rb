f4d2b91e673c542d75b346cd18fdc288f0de49a7
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-26 13:34:41 +0000

    [create]customer/edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,12 +11,12 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to admin_customer_path(@customer.id)
+    redirect_to customer_path(@customer.id)
   end
-  
-  
+
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
