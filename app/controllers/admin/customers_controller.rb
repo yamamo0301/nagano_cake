5a6b3a010292e87299462f461dab39101b61b3e9
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-26 09:47:07 +0000

    [create]admin/customers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,17 +1,17 @@
 class Admin::CustomersController < ApplicationController
   def index
-  
+    @customers = Customer.all
   end
-  
+
   def show
-    
+    @customer = Customer.find(params[:id])
   end
-  
+
   def edit
-  
+
   end
-  
+
   def update
-  
+
   end
 end
