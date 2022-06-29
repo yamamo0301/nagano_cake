9f97f7917677875ffd7256c628b11b18eb17e9b0
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-29 11:26:59 +0000

    [create]addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,44 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses.all
+  end
+
+  def create
+    @address = current_customer.addresses.new(address_params)
+    if @address.save
+      flash[:notice] = '新規配送先を追加しました。'
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = '配送先の内容を変更しました。'
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses.all
+      render :index
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
 end
