3a0775eb0cc4a69e41ed8ccecbc3096f8615ef4f
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-07-05 06:49:15 +0000

    [create]cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,7 +7,12 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    if @cart_item.save
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
       flash[:notice] = '商品を追加しました。'
       redirect_to cart_items_path
     else
